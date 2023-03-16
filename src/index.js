414b41123744cca77a5ecc5fb74869188706072e
rice <71971658+riceecakee@users.noreply.github.com>
2023-03-16 09:03:25 +0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  $(".datepicker").datepicker({
+    nextText: '<i class="fa fa-fw fa-angle-right"></i>',
+    prevText: '<i class="fa fa-fw fa-angle-left"></i>'
+  });
+});
+
